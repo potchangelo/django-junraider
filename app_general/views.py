6a65ef95adf0dec2dc8c4e33cb5b0ac0187a1385
Potchara Puttawanchai <potchangelo@gmail.com>
2021-12-04 16:49:49 +0700

    Remove ;

--- app_general/views.py
+++ app_general/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'app_general/home.html');
+    return render(request, 'app_general/home.html')
 
 def about(request):
     return render(request, 'app_general/about.html')
