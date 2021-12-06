cb6e53d59823dd88da9c1bc8502983840c8904f5
Potchara Puttawanchai <potchangelo@gmail.com>
2021-12-06 16:26:25 +0700

    templates x tags for app_foods 1

--- app_foods/views.py
+++ app_foods/views.py
@@ -1,9 +1,16 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 
+all_foods = [
+    { 'id': 1, 'title': 'Red Spicy', 'price': 299, 'isPremium': False },
+    { 'id': 2, 'title': 'Blue Iceberg', 'price': 299, 'isPremium': False },
+    { 'id': 3, 'title': 'Premium Black Choco', 'price': 399, 'isPremium': True },
+]
+
 # Create your views here.
 def foods(request):
-    return render(request, 'app_foods/foods.html')
+    context = { 'foods': all_foods }
+    return render(request, 'app_foods/foods.html', context)
 
 def food(request, food_id):
     return render(request, 'app_foods/food.html')
