6539a4032b9e838cc6bf5c24a47d055bb0662845
Potchara Puttawanchai <potchangelo@gmail.com>
2021-12-07 17:19:12 +0700

    models x migrations

--- app_foods/models.py
+++ app_foods/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Food(models.Model):
+    title = models.CharField(max_length=60)
+    price = models.IntegerField()
+    isPremium = models.BooleanField(default=False)
+    promotionEndAt = models.DateTimeField(null=True)
