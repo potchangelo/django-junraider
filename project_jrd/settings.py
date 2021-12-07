50663da91d04653f2609f3e6eabb9d6ce3b3444c
Potchara Puttawanchai <potchangelo@gmail.com>
2021-12-07 16:33:53 +0700

    Install mysql-connector-python

--- project_jrd/settings.py
+++ project_jrd/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'project_jrd.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'django_junraider',
+        'USER': 'root',
+        'PASSWORD': 'password1234',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
     }
 }
 
