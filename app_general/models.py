9a0381a3a70613f67307aed8a949dcd8e8c37303
Potchara Puttawanchai <potchangelo@gmail.com>
2021-12-10 12:34:19 +0700

    models x migrations for app_general

--- app_general/models.py
+++ app_general/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Subscription(models.Model):
+    class Status(models.TextChoices):
+        UNAPPROVED = 'unapproved',
+        BANNED = 'banned',
+        APPROVED = 'approved'
+
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50, unique=True)
+    status = models.CharField(max_length=15, choices=Status.choices, default=Status.UNAPPROVED)
+    registeredAt = models.DateTimeField(auto_now_add=True)
