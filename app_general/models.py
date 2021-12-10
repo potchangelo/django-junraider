b0fd4c20ffa0da85da92551286c2bce97322ea3a
Potchara Puttawanchai <potchangelo@gmail.com>
2021-12-10 17:50:46 +0700

    Choices that worked with mysql-connector-python

--- app_general/models.py
+++ app_general/models.py
@@ -2,13 +2,16 @@ from django.db import models
 
 # Create your models here.
 
+
 class Subscription(models.Model):
-    class Status(models.TextChoices):
-        UNAPPROVED = 'unapproved',
-        BANNED = 'banned',
-        APPROVED = 'approved'
+    STATUS = [
+        ('unapproved', 'Unapproved'),
+        ('banned', 'Banned'),
+        ('approved', 'Approved')
+    ]
 
     name = models.CharField(max_length=50)
     email = models.EmailField(max_length=50, unique=True)
-    status = models.CharField(max_length=15, choices=Status.choices, default=Status.UNAPPROVED)
+    status = models.CharField(
+        max_length=15, choices=STATUS, default='unapproved')
     registered_at = models.DateTimeField(auto_now_add=True)
