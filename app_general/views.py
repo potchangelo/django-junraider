9749fb6e28641c3bd127f2a042c0e17cde526089
Potchara Puttawanchai <potchangelo@gmail.com>
2021-12-16 16:57:52 +0700

    Normal form

--- app_general/views.py
+++ app_general/views.py
@@ -1,5 +1,8 @@
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from app_foods.models import Food
+from .models import Subscription
 
 # Create your views here.
 
@@ -10,6 +13,29 @@ def about(request):
     return render(request, 'app_general/about.html')
 
 def subscription(request):
+    # POST
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        food_ids = request.POST.getlist('food_id')
+        accepted = request.POST.get('accepted')
+
+        if accepted == 'yes':
+            new_sub = Subscription(name=name, email=email)
+            new_sub.save()
+            if len(food_ids) > 0:
+                selected_foods = Food.objects.filter(id__in=food_ids)
+                new_sub.food_set.set(selected_foods)
+            return HttpResponseRedirect(reverse('subscription_thankyou'))
+        
+        all_foods = Food.objects.order_by('-is_premium')
+        context = {
+            'foods': all_foods, 'please_accepted': 'กรุณาติ๊กถูกตรงข้อตกลงเพิ่มเติมด้วย', 
+            'values': request.POST, 'food_ids': list(map(int, food_ids))
+        }
+        return render(request, 'app_general/subscription_form.html', context)
+
+    # GET
     all_foods = Food.objects.order_by('-is_premium')
     context = {'foods': all_foods}
     return render(request, 'app_general/subscription_form.html', context)
