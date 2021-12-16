f820243e3ed4529da98a9022135b92842b48b156
Potchara Puttawanchai <potchangelo@gmail.com>
2021-12-16 15:14:25 +0700

    General content

--- app_general/views.py
+++ app_general/views.py
@@ -2,6 +2,7 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
 def home(request):
     return render(request, 'app_general/home.html')
 
