289e852402ce2b281f63af9e42127c71d786b59d
Potchara Puttawanchai <potchangelo@gmail.com>
2021-12-16 14:51:45 +0700

    models x views x templates for foods page

--- app_foods/views.py
+++ app_foods/views.py
@@ -1,35 +1,17 @@
 from datetime import datetime
 from django.http.response import HttpResponse
 from django.shortcuts import render
-
-all_foods = [
-    {
-        'id': 1, 'title': 'Red Spicy', 'price': 2990, 'isPremium': False,
-        'promotionEndAt': datetime(2022, 1, 31)
-    },
-    {
-        'id': 2, 'title': 'Blue Iceberg', 'price': 2990, 'isPremium': False,
-        'promotionEndAt': datetime(2022, 2, 15)
-    },
-    {
-        'id': 3, 'title': 'Premium Black Choco', 'price': 3990, 'isPremium': True,
-        'promotionEndAt': datetime(2022, 2, 28)
-    },
-]
+from .models import Food
 
 # Create your views here.
 
-
 def foods(request):
+    all_foods = Food.objects.order_by('-is_premium')
     context = {'foods': all_foods}
     return render(request, 'app_foods/foods.html', context)
 
 
 def food(request, food_id):
     one_food = None
-    try:
-        one_food = [f for f in all_foods if f['id'] == food_id][0]
-    except IndexError:
-        print('ไม่มีเธอ ไม่เจอ Index')
     context = {'food': one_food}
     return render(request, 'app_foods/food.html', context)
