6e4ad99fb0cc527e6e72bf9d16db69d47668d76e
Potchara Puttawanchai <potchangelo@gmail.com>
2021-12-16 15:36:26 +0700

    models x views x templates for subscription page

--- app_general/urls.py
+++ app_general/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about', views.about, name='about'),
+    path('subscription', views.subscription, name='subscription'),
+    path('subscription/thankyou', views.subscription_thankyou, name='subscription_thankyou'),
 ]
--- app_general/views.py
+++ app_general/views.py
@@ -1,5 +1,5 @@
-from django.http.response import HttpResponse
 from django.shortcuts import render
+from app_foods.models import Food
 
 # Create your views here.
 
@@ -8,3 +8,11 @@ def home(request):
 
 def about(request):
     return render(request, 'app_general/about.html')
+
+def subscription(request):
+    all_foods = Food.objects.order_by('-is_premium')
+    context = {'foods': all_foods}
+    return render(request, 'app_general/subscription_form.html', context)
+
+def subscription_thankyou(request):
+    return render(request, 'app_general/subscription_thankyou.html')
