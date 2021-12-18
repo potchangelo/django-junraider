ec468a8e9bee81188619ab0859c248fafa81408b
Potchara Puttawanchai <potchangelo@gmail.com>
2021-12-18 16:15:42 +0700

    Food admin page

--- app_foods/admin.py
+++ app_foods/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from app_foods.models import Food
 
 # Register your models here.
+
+class FoodAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price', 'special_price', 'is_premium', 'promotion_end_at']
+    search_fields = ['title']
+    list_filter = ['is_premium']
+
+admin.site.register(Food, FoodAdmin)
