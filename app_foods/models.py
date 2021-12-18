1d8c25c9d361923ab145ea9708fff3a4ccf89c6d
Potchara Puttawanchai <potchangelo@gmail.com>
2021-12-18 16:07:45 +0700

    Migration blank=True for optional inputs on admin site

--- app_foods/models.py
+++ app_foods/models.py
@@ -5,7 +5,10 @@ from django.db import models
 class Food(models.Model):
     title = models.CharField(max_length=60)
     price = models.IntegerField()
-    special_price = models.IntegerField(null=True)
+    special_price = models.IntegerField(null=True, blank=True)
     is_premium = models.BooleanField(default=False)
-    promotion_end_at = models.DateTimeField(null=True)
-    description = models.TextField(null=True)
+    promotion_end_at = models.DateTimeField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return '{} (id: {})'.format(self.title, self.id)
