fa77e59461fedadc971ee3892d54baeb1236bc1c
Potchara Puttawanchai <potchangelo@gmail.com>
2021-12-21 17:02:13 +0700

    Static JavaScript for food set checkbox validation

---
+++ app_general/static/app_general/main.js
@@ -0,0 +1,15 @@
+console.log('จันทร์ไรเดอร์');
+
+const subscriptionForm = document.querySelector('.subscription-form');
+
+function foodSetValidation(event) {
+    const checkedFoodSet = document.querySelectorAll('input[name="food_set"]:checked');
+    if (checkedFoodSet.length === 0) {
+        event.preventDefault();
+        alert('กรุณาเลือกเมนูอย่างน้อย 1 เมนู');
+    }
+}
+
+if (!!subscriptionForm) {
+    subscriptionForm.addEventListener('submit', foodSetValidation);
+}
