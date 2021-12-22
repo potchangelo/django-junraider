005ccb30d6b5e4277b9a2ff12cac50d63d3aaf78
Potchara Puttawanchai <potchangelo@gmail.com>
2021-12-22 16:04:15 +0700

    SECRET_KEY on .env

--- project_jrd/settings.py
+++ project_jrd/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d!p!su*34fri)rn^(00!dz9xy3!8*-%!s1==mi*m_*#me!5-ms'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
@@ -128,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
