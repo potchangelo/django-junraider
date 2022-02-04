3fc7354bbdd492aabcc041c6af5a883017c8a304
Potchara Puttawanchai <potchangelo@gmail.com>
2022-02-04 18:14:41 +0700

    Absolute import for app foods

--- app_foods/urls.py
+++ app_foods/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views
+from app_foods import views
 
 urlpatterns = [
     path('', views.foods, name='foods'),
--- app_foods/views.py
+++ app_foods/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Food
+from app_foods.models import Food
 
 # Create your views here.
 
