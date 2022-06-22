49cd19ff058a5670a5cf82b557904b401e7c8bf6
Potchara Puttawanchai <potchangelo@gmail.com>
2022-06-22 15:20:53 +0700

    Profile model

--- app_users/models.py
+++ app_users/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    address = models.TextField(default="", blank=True)
+    phone = models.CharField(max_length=15, default="", blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
