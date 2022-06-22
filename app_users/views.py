7aa91e8c183ddf346ea73e296dcdb0cb5930ed03
Potchara Puttawanchai <potchangelo@gmail.com>
2022-06-22 13:14:51 +0700

    Register form POST handle

--- app_users/views.py
+++ app_users/views.py
@@ -1,10 +1,23 @@
-from django.http import HttpRequest
+from django.contrib.auth import login
+from django.http import HttpRequest, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 from app_users.forms import RegisterForm
 
 # Create your views here.
 def register(request: HttpRequest):
-    form = RegisterForm()
+    # POST
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            # Register & Login
+            user = form.save()
+            login(request, user)
+            return HttpResponseRedirect(reverse("home"))
+    else:
+        form = RegisterForm()
+
+    # GET
     context = {"form": form}
     return render(request, "app_users/register.html", context)
