b61f2764fe96e540436c1062d8663a4afdb76232
Potchara Puttawanchai <potchangelo@gmail.com>
2022-06-22 12:32:47 +0700

    Login redirect url

--- project_jrd/settings.py
+++ project_jrd/settings.py
@@ -133,7 +133,13 @@ STATIC_URL = "static/"
 STATIC_ROOT = "static/"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Authentication
+
+LOGIN_REDIRECT_URL = "home"
