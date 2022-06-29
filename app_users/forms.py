655d104dd1aaaf7036e5f17b5f45e12e5cf05904
Potchara Puttawanchai <potchangelo@gmail.com>
2022-06-29 17:27:54 +0700

    Forms by CustomUser

--- app_users/forms.py
+++ app_users/forms.py
@@ -2,17 +2,18 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
-from app_users.models import Profile
+from app_users.models import CustomUser, Profile
 
 
 class RegisterForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
+        model = CustomUser
         fields = UserCreationForm.Meta.fields + ("email", )
 
 
 class UserProfileForm(forms.ModelForm):
     class Meta:
-        model = User
+        model = CustomUser
         fields = ("first_name", "last_name")
 
 
