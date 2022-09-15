19c9afa577223e83c53cfc6c14c55cac3c846d1a
Potchara Puttawanchai <potchangelo@gmail.com>
2022-09-15 16:49:01 +0700

    Add support for PostgreSQL

--- project_jrd/settings.py
+++ project_jrd/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = "project_jrd.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "mysql.connector.django",
+        "ENGINE": os.getenv("DB_ENGINE", ""),
         "NAME": os.getenv("DB_NAME", ""),
         "USER": os.getenv("DB_USER", ""),
         "PASSWORD": os.getenv("DB_PASS", ""),
